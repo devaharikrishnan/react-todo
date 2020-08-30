dbfbb91508586774e1bbc4e0c94f2fe8ac593cc4
devaharikrishnan <k.devahari@gmail.com>
2020-08-30 11:32:39 +0530

    first commit

---
+++ src/myinfo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function MyApp() {
+	return (
+	<div>
+	<h4>cat 1</h4>
+	<img alt="test" src="https://undark.org/wp-content/uploads/2020/02/GettyImages-1199242002-1-scaled.jpg" height="100" width="100"/>
+	<p>dsf sgrt rds fgd</p>
+	<h4>cat 1</h4>
+	<img alt="test" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-cat-wearing-sunglasses-while-sitting-royalty-free-image-1571755145.jpg"  height="100" width="100"/>
+	<p>dsf sgrt rds fgd</p>
+	<h4>cat 1</h4>
+	<img alt="test" src="https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2018/08/kitten-440379.jpg?h=c8d00152&itok=1fdekAh2"  height="100" width="100"/>
+	<p>dsf sgrt rds fgd</p>
+	<h4>cat 1</h4>
+	<img alt="test" src="https://img.rasset.ie/0014a2a1-500.jpg"  height="100" width="100"/>
+	<p>dsf sgrt rds fgd</p>
+    </div>
+    )
+}
+
+export default MyApp;
---
+++ src/test.js
@@ -0,0 +1,11 @@
+var s = "test@test.com";
+var l = s.split('@');
+var li = l[1].split('.');
+console.log(length(l))
+// if (l[1] == "test.com"){
+// 	console.log("it is valid");
+// }else{
+// 	console.log("it is not valid");
+// }
+
+
